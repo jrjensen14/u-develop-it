d3590f3092b89af52e1c737bff8bd540d3210713
JRJ <jjsocc171@msn.com>
2021-01-26 18:51:51 -0600

    created GET routes

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 // Connect to database
 const db = new sqlite3.Database('./db/election.db', err => {
     if (err) {
-      return console.error(err.message);
+        return console.error(err.message);
     }
-  
+
     console.log('Connected to the election database.');
-  });
+});
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -23,17 +23,83 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     });
 // });
 
+// return ALL the data in the candidates table
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// // Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
+
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
-  res.status(404).end();
+    res.status(404).end();
 });
 
 // Start server after DB connection
 db.on('open', () => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${PORT}`);
     });
-  });
+});
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
